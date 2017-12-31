0b5eb4265ec0811167cec26e167af9b759b2b41b
Jeremiah Buddenhagen <bitspill+github@bitspill.net>
2017-12-31 15:34:20 -0600

    Block reward

--- src/validation.cpp
+++ src/validation.cpp
@@ -1024,8 +1024,8 @@ CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
     if (halvings >= 64)
         return 0;
 
-    CAmount nSubsidy = 50 * COIN;
-    // Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
+    CAmount nSubsidy = 100 * COIN;
+    // Subsidy is cut in half every 800,000 blocks which will occur approximately every year.
     nSubsidy >>= halvings;
     return nSubsidy;
 }
