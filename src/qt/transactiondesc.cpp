4bd658ea6d52dff0194851d29f88413d1fce6c99
Jeremiah Buddenhagen <bitspill+git@bitspill.net>
2018-01-25 17:09:59 -0800

    Add tx comment to transaction details

--- src/qt/transactiondesc.cpp
+++ src/qt/transactiondesc.cpp
@@ -270,6 +270,10 @@ QString TransactionDesc::toHTML(CWallet *wallet, CWalletTx &wtx, TransactionReco
         strHTML += "<br>" + tr("Generated coins must mature %1 blocks before they can be spent. When you generated this block, it was broadcast to the network to be added to the block chain. If it fails to get into the chain, its state will change to \"not accepted\" and it won't be spendable. This may occasionally happen if another node generates a block within a few seconds of yours.").arg(QString::number(numBlocksToMaturity)) + "<br>";
     }
 
+    // Transaction Comment
+    if (!wtx.tx->strTxComment.empty())
+        strHTML += "<br><b>" + tr("Transaction Comment") + ":</b><br>" + GUIUtil::HtmlEscape(wtx.tx->strTxComment, true) + "<br>";
+
     //
     // Debug view
     //
