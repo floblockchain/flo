d5b57e70afb408209870b5968f0383419067772e
Ray Engelking <ray_engelking@hotmail.com>
2018-02-06 18:52:59 -0500

    copyright in .cpp, .c, and .h files updated with flo copyright

--- src/bench/prevector_destructor.cpp
+++ src/bench/prevector_destructor.cpp
@@ -1,4 +1,5 @@
 // Copyright (c) 2015-2017 The Bitcoin Core developers
+// Copyright (c) Flo Developers 2013-2018
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
