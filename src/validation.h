818b91b454342007ea36ba6fd2ab79a32203745a
Sky Young <OstlerDev@users.noreply.github.com>
2019-02-04 11:24:11 -0700

    Set Ancestor/Descendant Size limit to 1.75MB
    Rational for 1.75MB limit: https://github.com/floblockchain/flo/pull/8#issuecomment-458666055
    Comment documenting testing of values: https://github.com/floblockchain/flo/pull/8#issuecomment-460356226

--- src/validation.h
+++ src/validation.h
@@ -61,11 +61,11 @@ static const CAmount HIGH_MAX_TX_FEE = 100 * HIGH_TX_FEE_PER_KB;
 /** Default for -limitancestorcount, max number of in-mempool ancestors */
 static const unsigned int DEFAULT_ANCESTOR_LIMIT = 1250;
 /** Default for -limitancestorsize, maximum kilobytes of tx + all in-mempool ancestors */
-static const unsigned int DEFAULT_ANCESTOR_SIZE_LIMIT = 5050;
+static const unsigned int DEFAULT_ANCESTOR_SIZE_LIMIT = 1750;
 /** Default for -limitdescendantcount, max number of in-mempool descendants */
 static const unsigned int DEFAULT_DESCENDANT_LIMIT = 1250;
 /** Default for -limitdescendantsize, maximum kilobytes of in-mempool descendants */
-static const unsigned int DEFAULT_DESCENDANT_SIZE_LIMIT = 5050;
+static const unsigned int DEFAULT_DESCENDANT_SIZE_LIMIT = 1750;
 /** Default for -mempoolexpiry, expiration time for mempool transactions in hours */
 static const unsigned int DEFAULT_MEMPOOL_EXPIRY = 336;
 /** Maximum kilobytes for transactions to store for processing during reorg */
